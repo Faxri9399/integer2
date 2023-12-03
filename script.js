20d0292d5aded80116e6d9f9038179522c6a76d2
TEXNO <fbbozorov@gmail.com>
2023-12-03 14:41:22 +0500

    integer-vazifa2

---
+++ script.js
@@ -0,0 +1,68 @@
+//intege-11
+/*let a = 23;
+x = 2; y = 3;
+console.log(x+y);
+console.log(x*y);*/
+
+//integer-12
+/*let n = 12; 
+m = 2*(Math.pow(n+3, 2));
+console.log(m);*/
+
+//integer-13
+/*let n = 5;
+m = Math.pow((3+n)/2,2);
+console.log(m);*/
+
+//integer-14
+/*let n = 4;
+let x = 2;
+k = Math.pow(n,x)+Math.pow(6,x);
+console.log(k);*/
+
+//integer-15
+/*let a = -8;
+b = Math.abs(a);
+console.log(b);*/
+
+/*let a = -56;
+let b = Math.abs(a);
+console.log(b);*/
+
+/*let a = -45;
+let b = Math.abs(a);
+console.log(b);*/
+
+//integer-16
+/*let m = 3.456;
+k = Math.round(m);
+console.log(k.toFixed(2));*/
+/*let m = 5.678
+k = Math.round(m);
+console.log(k);
+let m = 7.5
+k = Math.round(m);
+console.log(k);*/
+//integer-17
+/*let x = 2;
+let y = 3;
+z = Math.pow(x,4)+5*Math.pow(x,2)+Math.pow(x,3)*y;
+console.log(z);*/
+//integer-18
+/*let x = 2;
+let y = 3;
+z=6*Math.pow(x,3)*Math.pow(y,5)+4*Math.pow(x,4)*Math.pow(y,3)-24*x*y;
+console.log(z);*/
+//integer-19
+/*let a = 230;
+x=2; y= 3; z=0;
+console.log(x+y+z);
+console.log(x*y*z);*/
+/*let a = 241;
+x=2; y=4; z=1;
+console.log(x+y+z);
+console.log(x*y*z);*/
+//integer-20
+/*let a = (1+2/3)-(3+1/4);
+b=Math.abs(a)
+console.log(b);*/
